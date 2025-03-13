040de678878fcf37211c2bcf2a466ff64ff09b3f
Wassim <w.elmahtouchi@student.fontys.nl>
2025-03-13 10:56:38 +0100

    initial commit

--- main.go
+++ main.go
@@ -7,21 +7,42 @@ import (
 
 func main() {
 	hour := time.Now().Hour()
-	//als de tijd lager is dan 7 dan foutmelding geven "We zijn helaas gesloten!"
-	if hour < 7 {
-		fmt.Println("Sorry, de parkeerplaats is ’s nachts gesloten!")
-	}
-	//als de tijd tussen 7 en 12 in zit dan melding geven "Goedemorgen"
-	if hour >= 7 && hour <= 12 {
-		fmt.Println("Goedemorgen!")
-	}
-	//als de tijd tussen 12 en 18 zit dan melding geven "Goedemiddag"
-	if hour >= 13 && hour <= 18 {
-		fmt.Println("Goedemiddag!")
-	}
-	//als de tijd tussen 18 en 23 zit dan melding geven "goedeavond"
-	if hour >= 19 && hour <= 23 {
-		fmt.Println("Goedeavond!")
+
+	var Kentekens [5]string
+	Kentekens[0] = "AB-12-34"
+	Kentekens[1] = "12-34-CD"
+	Kentekens[2] = "XY-56-78"
+	Kentekens[3] = "90-AB-12"
+	Kentekens[4] = "CD-34-56"
+
+	fmt.Print("Voer alstublieft uw naam in: ")
+	var Naam string
+	fmt.Scan(&Naam)
+
+	fmt.Print("Voer alstublieft uw kenteken in (xx-xx-xx): ")
+	var KentekenInput string
+	fmt.Scan(&KentekenInput)
+
+	if KentekenInput == Kentekens[0] || KentekenInput == Kentekens[1] || KentekenInput == Kentekens[2] || KentekenInput == Kentekens[3] || KentekenInput == Kentekens[4] {
+		//als de tijd lager is dan 7 dan foutmelding geven "We zijn helaas gesloten!"
+		if hour < 7 {
+			fmt.Println("Sorry,", Naam, "de parkeerplaats is ’s nachts gesloten!")
+		}
+		//als de tijd tussen 7 en 12 in zit dan melding geven "Goedemorgen"
+		if hour >= 7 && hour <= 12 {
+			fmt.Println("Goedemorgen", Naam)
+		}
+		//als de tijd tussen 12 en 18 zit dan melding geven "Goedemiddag"
+		if hour >= 13 && hour <= 18 {
+			fmt.Println("Goedemiddag", Naam)
+		}
+		//als de tijd tussen 18 en 23 zit dan melding geven "goedeavond"
+		if hour >= 19 && hour <= 23 {
+			fmt.Println("Goedeavond!", Naam)
+		} else {
+			fmt.Println("Sorry,", Naam, "Uw kenteken staat niet geregistreerd in ons lijst!")
+
+		}
 	}
 }
 
